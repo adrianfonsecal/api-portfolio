7b63d81b72ecdd5c32bd19281aa1242d639a63fc
Adrian Fonseca <adrianfonsecaloria@gmail.com>
2023-02-02 14:49:18 -0600

    modifying module django_on_heroku

--- apicrud/settings.py
+++ apicrud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_heroku
+import django_on_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
